15f2b7c220546529dfcbf476fca16a191b5d8f7d
indante <koreanhoseon@gmail.com>
2022-04-29 02:04:57 +0900

    Update test code
    TDD 사용해보기

--- src/evaluate.test.ts
+++ src/evaluate.test.ts
@@ -1,13 +1,15 @@
-import { evaluate } from "./evaluate"
+import { evaluate } from "./evaluate";
 
-describe('evalute', () => {
-  it('기본 사칙연산이 잘 동작한다', () => {
-    expect(evaluate('1+2')).toEqual(3)
-  })
+describe("evalute", () => {
+  it("기본 사칙연산이 잘 동작한다", () => {
+    expect(evaluate("1+2")).toEqual(3);
+  });
 
-  it('입출력 예시가 잘 동작해야 한다', () => {
-    expect(evaluate('1 + 2 * 3')).toEqual(7)
-    expect(evaluate('(1 + 2) * 3')).toEqual(9)
-    expect(evaluate('1 / 32.5 + 167 * (3498 - 1155) * -721 * (4885 - 1) / 0.5')).toEqual(-2755685654567.969)
-  })
-})
+  it("입출력 예시가 잘 동작해야 한다", () => {
+    expect(evaluate("1 + 2 * 3")).toEqual(7);
+    expect(evaluate("(1 + 2) * 3")).toEqual(9);
+    expect(
+      evaluate("1 / 32.5 + 167 * (3498 - 1155) * -721 * (4885 - 1) / 0.5")
+    ).toEqual(-2755685654567.969);
+  });
+});
---
+++ src/postfix.test.ts
@@ -0,0 +1,11 @@
+import { infixToPostfix } from "./postfix";
+
+describe("infixToPostfix", () => {
+  it("괄호 없는 경우", () => {
+    expect(infixToPostfix("1 + 2 * 3")).toEqual("123*+");
+  });
+
+  it("괄호 있는 경우", () => {
+    expect(infixToPostfix("(1 + 2) * 3")).toEqual("12+3*");
+  });
+});
